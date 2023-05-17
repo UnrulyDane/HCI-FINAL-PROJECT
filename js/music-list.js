29676144fbc59ca1b564e28fbee94079a375e48f
UnrulyDane <133210435+UnrulyDane@users.noreply.github.com>
2023-05-17 08:31:54 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "Armed & Dangerous",
+    artist: "Juice WRLD",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Shotta Flow 6 (Finale)",
+    artist: "NLE Choppa",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Dreams Unfold",
+    artist: "Joyner Lucas ft. Lil Tjay",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Self Sabotage",
+    artist: "Ericdoa",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Metamorphosis 2",
+    artist: "INTERWRLD",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Titles",
+    artist: "Twikipedia",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
